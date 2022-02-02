47a472d4c7ebc9c9ead2d66609fdc4cd44171daa
eyeq <eyeq800@gmail.com>
2022-02-02 22:54:33 +0900

    Update Item clickable

--- app/src/main/java/eyeq/alchemy/Item.kt
+++ app/src/main/java/eyeq/alchemy/Item.kt
@@ -3,16 +3,17 @@ package eyeq.alchemy
 import android.graphics.Color
 
 enum class Item(val resId: Int, val color: Int) {
+    EMPTY(R.drawable.item_empty, Color.parseColor("#000000")),
     ELEMENTAL_VOID(R.drawable.item_elemental, Color.parseColor("#808080")),
     ELEMENTAL_EARTH(R.drawable.item_elemental, Color.parseColor("#800000")),
     ELEMENTAL_WATER(R.drawable.item_elemental, Color.parseColor("#00FFFF")),
     ELEMENTAL_AIR(R.drawable.item_elemental, Color.parseColor("#FF80FF")),
     ELEMENTAL_FIRE(R.drawable.item_elemental, Color.parseColor("#FF8000")),
     ELEMENTAL_METAL(R.drawable.item_elemental, Color.parseColor("#FF0000")),
-    ELEMENTAL_ICE(R.drawable.item_elemental, Color.parseColor("#0000FF")),
+    ELEMENTAL_ICE(R.drawable.item_elemental, Color.parseColor("#FFFFFF")),
     ELEMENTAL_PLANT(R.drawable.item_elemental, Color.parseColor("#00FF80")),
     ELEMENTAL_THUNDER(R.drawable.item_elemental, Color.parseColor("#8000FF")),
     ELEMENTAL_LIGHT(R.drawable.item_elemental, Color.parseColor("#FFFF00")),
-    ELEMENTAL_DARK(R.drawable.item_elemental, Color.parseColor("#000000")),
+    ELEMENTAL_DARK(R.drawable.item_elemental, Color.parseColor("#0000FF")),
     ;
 }
--- app/src/main/java/eyeq/alchemy/MainActivity.kt
+++ app/src/main/java/eyeq/alchemy/MainActivity.kt
@@ -1,26 +1,88 @@
 package eyeq.alchemy
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import com.google.android.flexbox.FlexboxLayout
 
 class MainActivity : AppCompatActivity() {
+    private var item1 = Item.EMPTY
+    private var item2 = Item.EMPTY
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val layout = findViewById<FlexboxLayout>(R.id.flex)
-        for (item in Item.values()) {
-            val image = ImageView(this)
-            image.setImageResource(item.resId)
-            image.setColorFilter(item.color)
+        updateFlex()
+        updatePot()
+    }
 
-            var layoutParams: ViewGroup.MarginLayoutParams = ViewGroup.MarginLayoutParams(80.dpToPx(), 80.dpToPx())
-            layoutParams.setMargins(10.dpToPx(), 10.dpToPx(), 10.dpToPx(), 10.dpToPx())
+    private fun updateFlex() {
+        val layoutParams = ViewGroup.MarginLayoutParams(48.dpToPx(), 48.dpToPx())
+        layoutParams.setMargins(8.dpToPx(), 8.dpToPx(), 8.dpToPx(), 8.dpToPx())
+
+        val layout = findViewById<FlexboxLayout>(R.id.flex)
+        layout.removeAllViews()
+        layout.setBackgroundColor(Color.BLACK)
+
+        for (item in Item.values()) {
+            val image = generateImageView(item.resId, item.color)
+            image.isClickable = true
+            image.setOnClickListener { view ->
+                if (item1 == Item.EMPTY) {
+                    item1 = item
+                } else if (item2 == Item.EMPTY) {
+                    item2 = item
+                }
+                updatePot()
+            }
             layout.addView(image, layoutParams)
         }
+    }
+
+    private fun updatePot() {
+        val layoutParams = ViewGroup.MarginLayoutParams(48.dpToPx(), 48.dpToPx())
+        layoutParams.setMargins(2.dpToPx(), 2.dpToPx(), 2.dpToPx(), 2.dpToPx())
+
+        val layout = findViewById<LinearLayout>(R.id.pot)
+        layout.removeAllViews()
+
+        val image1 = generateImageView(item1.resId, item1.color)
+        image1.isClickable = true
+        image1.setOnClickListener { view ->
+            item1 = Item.EMPTY
+            updatePot()
+        }
+        layout.addView(image1, layoutParams)
+
+        layout.addView(generateImageView(R.drawable.symbol_plus, Color.WHITE), layoutParams)
+
+        val image2 = generateImageView(item2.resId, item2.color)
+        image2.isClickable = true
+        image2.setOnClickListener { view ->
+            item2 = Item.EMPTY
+            updatePot()
+        }
+        layout.addView(image2, layoutParams)
+
+        layout.addView(generateImageView(R.drawable.symbol_arrow, Color.WHITE), layoutParams)
+
+        val reload = generateImageView(R.drawable.symbol_reload, Color.WHITE)
+        reload.isClickable = true
+        reload.setOnClickListener { view ->
+
+        }
+        layout.addView(reload, layoutParams)
+    }
+
+    private fun generateImageView(resId: Int, color: Int): ImageView {
+        val image = ImageView(this)
+        image.setImageResource(resId)
+        image.setColorFilter(color)
+        return image
     }
 
     private fun Int.pxToDp(): Int = (this / resources.displayMetrics.density).toInt()
