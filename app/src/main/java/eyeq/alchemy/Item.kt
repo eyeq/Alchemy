683ca7caf8ae861254ba1bd85e2e59c3463d7ba2
eyeq <eyeq800@gmail.com>
2022-02-05 16:59:02 +0900

    Update Recipe

--- app/src/main/java/eyeq/alchemy/Item.kt
+++ app/src/main/java/eyeq/alchemy/Item.kt
@@ -2,6 +2,7 @@ package eyeq.alchemy
 
 enum class Item(val resId: Int, val colorId: Int, val textId: Int) {
     EMPTY(R.drawable.item_empty, R.color.black, R.string.item_empty),
+
     ELEMENTAL_VOID(R.drawable.item_elemental, R.color.gray, R.string.item_elemental_void),
     ELEMENTAL_EARTH(R.drawable.item_elemental, R.color.bluish_green, R.string.item_elemental_earth),
     ELEMENTAL_WATER(R.drawable.item_elemental, R.color.cyan, R.string.item_elemental_water),
@@ -11,7 +12,18 @@ enum class Item(val resId: Int, val colorId: Int, val textId: Int) {
     ELEMENTAL_ICE(R.drawable.item_elemental, R.color.white, R.string.item_elemental_ice),
     ELEMENTAL_PLANT(R.drawable.item_elemental, R.color.lime, R.string.item_elemental_plant),
     ELEMENTAL_THUNDER(R.drawable.item_elemental, R.color.reddish_blue, R.string.item_elemental_thunder),
-    ELEMENTAL_LIGHT(R.drawable.item_elemental, R.color.yellow, R.string.item_elemental_light),
     ELEMENTAL_DARK(R.drawable.item_elemental, R.color.blue, R.string.item_elemental_dark),
+    ELEMENTAL_LIGHT(R.drawable.item_elemental, R.color.yellow, R.string.item_elemental_light),
+
+    EARTH(R.drawable.item_earth, R.color.red_iron_oxide, R.string.item_earth),
+    WATER(R.drawable.item_water, R.color.cyan, R.string.item_water),
+    AIR(R.drawable.item_air, R.color.bluish_green, R.string.item_air),
+    FIRE(R.drawable.item_fire, R.color.greenish_red, R.string.item_fire),
+    METAL(R.drawable.item_metal, R.color.gray, R.string.item_metal),
+    ICE(R.drawable.item_ice, R.color.white, R.string.item_ice),
+    PLANT(R.drawable.item_plant, R.color.lime, R.string.item_plant),
+    THUNDER(R.drawable.item_thunder, R.color.reddish_blue, R.string.item_thunder),
+    SPACE(R.drawable.item_space, R.color.blue, R.string.item_space),
+    TIME(R.drawable.item_time, R.color.yellow, R.string.item_time),
     ;
 }
--- app/src/main/java/eyeq/alchemy/Recipe.kt
+++ app/src/main/java/eyeq/alchemy/Recipe.kt
@@ -6,16 +6,29 @@ class Recipe(val result: Item, vararg inputs: Item) {
     companion object {
         val recipes = mutableListOf<Recipe>(
             Recipe(Item.ELEMENTAL_VOID),
+
             Recipe(Item.ELEMENTAL_EARTH, Item.ELEMENTAL_VOID, Item.ELEMENTAL_VOID),
             Recipe(Item.ELEMENTAL_WATER, Item.ELEMENTAL_VOID, Item.ELEMENTAL_VOID),
             Recipe(Item.ELEMENTAL_AIR, Item.ELEMENTAL_VOID, Item.ELEMENTAL_VOID),
             Recipe(Item.ELEMENTAL_FIRE, Item.ELEMENTAL_VOID, Item.ELEMENTAL_VOID),
+
             Recipe(Item.ELEMENTAL_METAL, Item.ELEMENTAL_EARTH, Item.ELEMENTAL_FIRE),
             Recipe(Item.ELEMENTAL_ICE, Item.ELEMENTAL_AIR, Item.ELEMENTAL_WATER),
             Recipe(Item.ELEMENTAL_PLANT, Item.ELEMENTAL_EARTH, Item.ELEMENTAL_WATER),
             Recipe(Item.ELEMENTAL_THUNDER, Item.ELEMENTAL_AIR, Item.ELEMENTAL_FIRE),
-            Recipe(Item.ELEMENTAL_LIGHT, Item.ELEMENTAL_WATER, Item.ELEMENTAL_FIRE),
             Recipe(Item.ELEMENTAL_DARK, Item.ELEMENTAL_EARTH, Item.ELEMENTAL_AIR),
+            Recipe(Item.ELEMENTAL_LIGHT, Item.ELEMENTAL_WATER, Item.ELEMENTAL_FIRE),
+
+            Recipe(Item.EARTH, Item.ELEMENTAL_VOID, Item.ELEMENTAL_EARTH),
+            Recipe(Item.WATER, Item.ELEMENTAL_VOID, Item.ELEMENTAL_WATER),
+            Recipe(Item.AIR, Item.ELEMENTAL_VOID, Item.ELEMENTAL_AIR),
+            Recipe(Item.FIRE, Item.ELEMENTAL_VOID, Item.ELEMENTAL_FIRE),
+            Recipe(Item.METAL, Item.ELEMENTAL_VOID, Item.ELEMENTAL_METAL),
+            Recipe(Item.ICE, Item.ELEMENTAL_VOID, Item.ELEMENTAL_ICE),
+            Recipe(Item.PLANT, Item.ELEMENTAL_VOID, Item.ELEMENTAL_PLANT),
+            Recipe(Item.THUNDER, Item.ELEMENTAL_VOID, Item.ELEMENTAL_THUNDER),
+            Recipe(Item.SPACE, Item.ELEMENTAL_VOID, Item.ELEMENTAL_DARK),
+            Recipe(Item.TIME, Item.ELEMENTAL_VOID, Item.ELEMENTAL_LIGHT),
         )
 
         fun alchemise(vararg inputs: Item): List<Recipe> {
