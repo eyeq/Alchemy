57be3a182ab0ff1cd5ea56403a4840bf62053164
eyeq <eyeq800@gmail.com>
2022-02-12 17:35:14 +0900

    Update Recipe

--- app/src/main/java/eyeq/alchemy/game/Recipe.kt
+++ app/src/main/java/eyeq/alchemy/game/Recipe.kt
@@ -20,15 +20,53 @@ class Recipe(val result: Item, vararg _inputs: Item) {
             Recipe(Item.ELEMENTAL_LIGHT, Item.ELEMENTAL_WATER, Item.ELEMENTAL_FIRE),
 
             Recipe(Item.EARTH, Item.ELEMENTAL_VOID, Item.ELEMENTAL_EARTH),
+            Recipe(Item.EARTH, Item.COLD, Item.DRY),
             Recipe(Item.WATER, Item.ELEMENTAL_VOID, Item.ELEMENTAL_WATER),
+            Recipe(Item.WATER, Item.COLD, Item.WET),
+            Recipe(Item.WATER, Item.STEAM, Item.COLD),
+            Recipe(Item.WATER, Item.ICE, Item.HOT),
             Recipe(Item.AIR, Item.ELEMENTAL_VOID, Item.ELEMENTAL_AIR),
+            Recipe(Item.AIR, Item.HOT, Item.WET),
             Recipe(Item.FIRE, Item.ELEMENTAL_VOID, Item.ELEMENTAL_FIRE),
+            Recipe(Item.FIRE, Item.HOT, Item.DRY),
             Recipe(Item.METAL, Item.ELEMENTAL_VOID, Item.ELEMENTAL_METAL),
             Recipe(Item.ICE, Item.ELEMENTAL_VOID, Item.ELEMENTAL_ICE),
             Recipe(Item.PLANT, Item.ELEMENTAL_VOID, Item.ELEMENTAL_PLANT),
             Recipe(Item.THUNDER, Item.ELEMENTAL_VOID, Item.ELEMENTAL_THUNDER),
             Recipe(Item.SPACE, Item.ELEMENTAL_VOID, Item.ELEMENTAL_DARK),
             Recipe(Item.TIME, Item.ELEMENTAL_VOID, Item.ELEMENTAL_LIGHT),
+
+            Recipe(Item.HOT, Item.AIR, Item.FIRE),
+            Recipe(Item.WET, Item.AIR, Item.WATER),
+            Recipe(Item.COLD, Item.EARTH, Item.WATER),
+            Recipe(Item.DRY, Item.EARTH, Item.FIRE),
+
+            Recipe(Item.VIBRATION, Item.HOT),
+
+            Recipe(Item.STONE, Item.EARTH, Item.EARTH),
+            Recipe(Item.MAGMA, Item.EARTH, Item.HOT),
+            Recipe(Item.MAGMA, Item.STONE, Item.HOT),
+            Recipe(Item.MUD, Item.EARTH, Item.WET),
+            Recipe(Item.SAND, Item.EARTH, Item.DRY),
+
+            Recipe(Item.STEAM, Item.WATER, Item.HOT),
+            Recipe(Item.ICE, Item.WATER, Item.COLD),
+            Recipe(Item.QUARTZ, Item.WATER, Item.DRY),
+
+            Recipe(Item.WIND, Item.AIR, Item.AIR),
+            Recipe(Item.SOUND, Item.AIR, Item.VIBRATION),
+
+            Recipe(Item.ELECTRICITY, Item.METAL, Item.THUNDER),
+            Recipe(Item.MAGNET, Item.METAL, Item.ELECTRICITY),
+
+            Recipe(Item.LEAF, Item.PLANT),
+            Recipe(Item.ASH, Item.PLANT, Item.FIRE),
+            Recipe(Item.LYE, Item.ASH, Item.WATER),
+
+            Recipe(Item.GRAVITY, Item.SPACE),
+            Recipe(Item.DIRECTION, Item.SPACE, Item.EARTH),
+            Recipe(Item.SKY, Item.SPACE, Item.EARTH),
+            Recipe(Item.STAR, Item.SPACE, Item.STONE),
         )
 
         fun alchemise(vararg inputs: Item): List<Recipe> {
