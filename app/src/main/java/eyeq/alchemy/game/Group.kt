2dcc112dbdda377214e70c903c1560faa01bef57
eyeq <eyeq800@gmail.com>
2022-02-27 21:38:15 +0900

    Update Recipe

--- app/src/main/java/eyeq/alchemy/game/Group.kt
+++ app/src/main/java/eyeq/alchemy/game/Group.kt
@@ -6,5 +6,6 @@ enum class Group(val textId: Int) {
     ALL(R.string.group_all),
     ELEMENTAL(R.string.group_elemental),
     MATERIAL(R.string.group_material),
+    NATURE(R.string.group_nature)
     ;
 }
