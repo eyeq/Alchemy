c70a3cdf11a79d59f53afc8d2cbe59d430e35f98
eyeq <eyeq800@gmail.com>
2022-03-13 20:23:19 +0900

    Update RecipeDialogFragment.kt Clipboard

--- app/src/main/java/eyeq/alchemy/ui/RecipeDialogFragment.kt
+++ app/src/main/java/eyeq/alchemy/ui/RecipeDialogFragment.kt
@@ -1,13 +1,17 @@
 package eyeq.alchemy.ui
 
 import android.app.Dialog
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ListView
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import eyeq.alchemy.R
 import eyeq.alchemy.game.History
 import eyeq.alchemy.game.Item
 import eyeq.alchemy.game.Recipe
@@ -17,6 +21,22 @@ class RecipeDialogFragment(private val recipeList: List<Recipe>, private val ite
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val context = requireContext()
 
+        val inflater = LayoutInflater.from(context)
+        val title = inflater.inflate(R.layout.view_item_title, null)
+
+        val titleText = title.findViewById<TextView>(R.id.text)
+
+        val titleButton = title.findViewById<ImageView>(R.id.image)
+        titleButton.isClickable = true
+        titleButton.setOnClickListener {
+            val text = titleText.text
+
+            val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            clipboard.setPrimaryClip(ClipData.newPlainText("", text))
+
+            Toast.makeText(context, "Copied; ${text}", Toast.LENGTH_SHORT).show();
+        }
+
         val adapter = HistoryAdapter()
         adapter.itemClickListener = object : HistoryAdapter.OnItemClickListener {
             override fun onClick(item: Item) {
@@ -24,7 +44,7 @@ class RecipeDialogFragment(private val recipeList: List<Recipe>, private val ite
                     itemStack.addFirst(item)
 
                     val dialog = dialog as? AlertDialog
-                    dialog?.setTitle(context.getString(item.textId))
+                    titleText.setText(item.textId)
                     dialog?.setMessage(context.getString(item.quoteId) + "\n")
                     adapter.setData(getHistoryList())
                 }
@@ -40,8 +60,10 @@ class RecipeDialogFragment(private val recipeList: List<Recipe>, private val ite
         view.addView(listView, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height))
 
         val item = itemStack.first()
+        titleText.setText(item.textId)
+
         val builder = AlertDialog.Builder(context)
-            .setTitle(context.getString(item.textId))
+            .setCustomTitle(title)
             .setMessage(context.getString(item.quoteId) + "\n")
             .setView(view)
             .setPositiveButton("close") { dialog, id -> }
